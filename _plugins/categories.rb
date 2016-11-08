82ed1f6573c10667b000e072952338cfc824b1e2
dssindustry <david.shaw@industry.gov.au>
2016-11-08 17:48:35 +1100

    latest changes

--- _plugins/categories.rb
+++ _plugins/categories.rb
@@ -3,26 +3,46 @@ require 'json'
 module Jekyll
 
   class QuestionPage < Page
-    def initialize(site, base, dir, pagedef)
+    def initialize(site, base, dir, pagedef, content)
       @site = site
       @base = base
       @dir = dir
-      @name = "question-#{pagedef['id']}.html"
-zz
+	  id = pagedef['id']
+	  id = id.rjust(2, '0')
+      @name = "question-#{id}.html"
+
       self.process(@name)
       self.read_yaml(File.join(base, '_layouts'), 'question.html')
+	  self.data['id'] = pagedef['id']
       self.data['title'] = pagedef['title']
 	  self.data['question'] = pagedef['question']
-	  self.data['next'] = "#{site.baseurl}/final.html" if (pagedef['next'] == "final")
-	  self.data['next'] = "question-#{pagedef['next']}.html" if (pagedef['next'] != "final")
+	  self.data['next'] = "#{site.baseurl}/summary.html" if (pagedef['next'] == "final")
+	  self.data['next'] = "question-#{pagedef['next'].rjust(2, '0')}.html" if (pagedef['next'] != "final")
+	  self.data['summary'] = content['summary']
+	  self.data['pdf'] = content['pdf']
+	  self.data['pdfDescription'] = content['pdfDescription']
+	  self.data['helpTitle'] = content['helpTitle']
+	  self.data['helpContent'] = content['helpContent']
     end
+  end
+  
+  class SummaryPage < Page
+	def initialize(site, base, content)
+		@site = site
+		@base = base
+		@name = "summary.html"
+		
+		self.process(@name)
+		self.read_yaml(File.join(base, '_layouts'), 'final.html')
+		self.data['contentitems'] = content
+	end
   end
 
   class CategoryPageGenerator < Generator
     safe true
 
     def generate(site)
-	  json_filename = site.config['data_product_file'] || 'pages.json'
+	  json_filename = 'pages.json'
       data_hash = site.read_data_object( json_filename ) # if File.exists?( json_filename )
       # json_mtime = data_hash['mtime'] if data_hash
       data = data_hash['data'] if data_hash
@@ -31,11 +51,28 @@ zz
         return "No data found in data file"
       end
       puts "## Pages file read: found #{pages.length} pages"
+	  
+	  json_filename = 'content.json'
+      data_hash = site.read_data_object( json_filename ) # if File.exists?( json_filename )
+      # json_mtime = data_hash['mtime'] if data_hash
+      data = data_hash['data'] if data_hash
+      content = data['Content'] if data
+      unless pages
+        return "No data found in data file"
+      end
+      puts "## Content file read: found content for #{content.length} pages"
 
+	  # build question pages:
 	  pages.each_with_index do |pagedef,index|
-	    puts "## #{pagedef['id']}: #{pagedef['title']} - #{pagedef['question']}"
-		site.pages << QuestionPage.new(site, site.source, "questions", pagedef)
+		if content[index] != nil
+			site.pages << QuestionPage.new(site, site.source, "questions", pagedef, content[index])
+		else
+			site.pages << QuestionPage.new(site, site.source, "questions", pagedef, "")
+		end
 	  end
+	  
+	  # build summary page:
+	  site.pages << SummaryPage.new(site, site.source, content)
     end
   end
 	
