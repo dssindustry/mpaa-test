6a1e0e25a1bf1ccaf1cd9fc34c3443c0d79a4dce
dssindustry <david.shaw@industry.gov.au>
2016-11-09 13:30:50 +1100

    latest

--- scripts/summary.js
+++ scripts/summary.js
@@ -1,3 +1,19 @@
+var status = 0;
+
 $(document).ready(function() {
+	var param = getURLParameter("s");
+	if (param != null) {
+		status = parseInt(param);
+	}
+	var top = Math.ceil(Math.log2(status)) + 1;
+	
+	for (var i=0; i<top; i++) {
+		var q = Math.pow(2, i);
+		if ((status & q) != 0) {
+			$("#row-" + (i+1)).show();
+			$("#help-" + (i+1)).show();
+		}
+	}
+	
 	initHelp();
 });
